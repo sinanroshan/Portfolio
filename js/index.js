746194a0b3c4227d7d2ef5be9bc6a23b422b43b2
Sinan Roshan <sinan@aufaitux.com>
2024-06-12 12:53:10 +0530

    home card updates & slider updates

--- js/index.js
+++ js/index.js
@@ -1,6 +1,43 @@
-// $(document).ready(function () {
+$(document).ready(function () {
+  const HomeSlider = document.getElementById("slider-home");
+  const sliderCard1 = document.getElementById("cardSlider1");
+  const sliderCard2 = document.getElementById("cardSlider2");
+  const sliderDetail = document.getElementById("slider-detail");
 
-// });
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        startScroll(entry);
+      }
+    });
+  });
+
+  observer.observe(HomeSlider);
+  observer.observe(sliderCard1);
+  observer.observe(sliderCard2);
+  observer.observe(sliderDetail);
+});
+
+function startScroll(sliderItem) {
+  if (!sliderItem.target.children[0]) {
+    return;
+  }
+  const sliderContent = sliderItem.target.children[0].firstElementChild;
+
+  sliderItem.target.children[0].appendChild(sliderContent.cloneNode(true));
+
+  const totalWidth = sliderContent.offsetWidth;
+
+  // Set the width of the marquee container
+  gsap.set(sliderItem.target.children[0], { width: totalWidth * 1.5 });
+
+  gsap.to(sliderItem.target.children[0], {
+    x: -totalWidth - 20,
+    duration: 40,
+    ease: "linear",
+    repeat: -2,
+  });
+}
 
 function redirectToPortfolio() {
   window.location.href = "/portfolio.html"; // Replace with your desired URL
