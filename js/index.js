d8bbf5f857b93d01170504146bb331e2359efce3
Sinan Roshan <sinan@aufaitux.com>
2024-09-10 23:39:46 +0530

    Feat: home card videos updated

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 function redirectToPortfolio() {
   window.location.href = "/portfolio.html";
 }
+
+function playVideo( card ) {
+  var iframe = card.querySelector( 'iframe' );
+  var player = new Vimeo.Player( iframe );
+  player.play();
+}
+
+function pauseVideo( card ) {
+  var iframe = card.querySelector( 'iframe' );
+  var player = new Vimeo.Player( iframe );
+  player.pause();
+}
