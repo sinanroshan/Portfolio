3e70f3c3cce20b3d50282c6549823943bf984d43
Sinan Roshan <sinan@aufaitux.com>
2024-09-28 17:35:21 +0530

    Feat : about page card description change

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 function redirectToPortfolio() {
   window.location.href = "/portfolio.html";
 }
+function openResume() {
+  var fileUrl = 'https://drive.google.com/drive/folders/1snRSbCG8mcUejGjTVNl0E2pL_O6_xzxe'
+  window.open( fileUrl, '_blank' );
+}
 
 function playVideo( card ) {
   var iframe = card.querySelector( 'iframe' );
